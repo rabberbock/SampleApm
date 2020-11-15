fe907f03110a24b7c2a0365b01d148667584b161
rabberbock <rabberbock@gmail.com>
2020-11-15 08:33:49 -0500

    come changes

---
+++ SampleApm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Elastic.Apm;
+using Elastic.Apm.Api;
+using Elastic.Apm.DiagnosticSource;
+using Elastic.Apm.EntityFrameworkCore;
+using Elastic.Apm.NetCoreAll;
+using Elastic.Apm.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApm
+{
+
+    public class Program {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseAllElasticApm()
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                    services.AddHostedService(sp =>
+                    {
+                        //if (!Agent.IsConfigured)
+                        //{
+                        //    Agent.Setup(sp.GetRequiredService<AgentComponents>());
+                        //    Agent.Subscribe(new HttpDiagnosticsSubscriber(),
+                        //                    new EfCoreDiagnosticsSubscriber(),
+                        //                    new SqlClientDiagnosticSubscriber());
+                        //}
+                        return new SampleService(sp.GetRequiredService<ILogger<SampleService>>(), sp.GetRequiredService<ITracer>());
+                    });
+
+
+                });
+        }
+}
---
+++ SampleApm/SampleService.cs
@@ -0,0 +1,44 @@
+﻿
+using Elastic.Apm.Api;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleApm
+{
+    public class SampleService : BackgroundService
+    {
+        private readonly ILogger<SampleService> _logger;
+        private ITracer _tracer;
+
+        public SampleService(ILogger<SampleService> logger, ITracer tracer)
+        {
+            _logger = logger;
+            _tracer = tracer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                await _tracer.CaptureTransaction("SampleHttpTransaction", ApiConstants.TypeRequest, async() =>
+                {
+                    var client = new HttpClient();
+                    HttpResponseMessage response = await client.GetAsync("http://www.example.com/");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+
+                    Console.WriteLine(responseBody);
+                   
+                });
+                await Task.Delay(10_000);
+            }
+
+        }
+    }
+}
